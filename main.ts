6d28d9e8b31370ec956e466d70b2e0384e6b1aaf
Torhild-Skagen <85964199+Torhild-Skagen@users.noreply.github.com>
2021-09-12 22:19:23 +0200

    Oppdater pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,14 @@
 bitbot.select_model(BBModel.XL)
 bitbot.ledRainbow()
-bitbot.ledRotate()
-bitbot.buzz(false)
 basic.forever(function () {
-    if (bitbot.readLine(BBLineSensor.Left) == 1 && bitbot.readLine(BBLineSensor.Right) == 0) {
-        bitbot.move(BBMotor.Left, BBDirection.Forward, 0)
-        bitbot.move(BBMotor.Right, BBDirection.Forward, 15)
-        bitbot.setLedColor(0x0000FF)
-    } else if (bitbot.readLine(BBLineSensor.Left) == 0 && bitbot.readLine(BBLineSensor.Right) == 1) {
-        bitbot.move(BBMotor.Left, BBDirection.Forward, 15)
-        bitbot.move(BBMotor.Right, BBDirection.Forward, 0)
+    if (bitbot.readLine(BBLineSensor.Left) == 1) {
         bitbot.setLedColor(0xFF0000)
+        bitbot.rotate(BBRobotDirection.Left, 10)
+    } else if (bitbot.readLine(BBLineSensor.Right) == 1) {
+        bitbot.setLedColor(0x0000FF)
+        bitbot.rotate(BBRobotDirection.Right, 10)
     } else {
-        bitbot.move(BBMotor.Both, BBDirection.Forward, 15)
+        bitbot.go(BBDirection.Forward, 25)
         bitbot.setLedColor(0x18E600)
     }
 })
